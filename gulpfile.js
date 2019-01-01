7ea2995e23a8a6e69ba7e7bb85531bd9f6fbd802
Frank Laemmer <fl@fortrabbit.de>
2019-01-01 13:32:08 +0100

    Updated gulpfiles for Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,53 @@
+
 // INIT plugins
 
-var gulp            = require('gulp');
-var sass            = require('gulp-sass');
-var prefix          = require('gulp-autoprefixer');
-var sourcemaps      = require('gulp-sourcemaps');
-var cleanCSS        = require('gulp-clean-css');
-var header          = require('gulp-header');
-var browserSync     = require('browser-sync').create();
+const gulp            = require('gulp');
+const sass            = require('gulp-sass');
+const prefix          = require('gulp-autoprefixer');
+const sourcemaps      = require('gulp-sourcemaps');
+const cleanCSS        = require('gulp-clean-css');
+const browserSync     = require('browser-sync').create();
 
 
 // VARIBALES
 
-var styleWatchDir   = '../teutonic-css/**/*.{css,scss}'; // All CSS & SCSS files to watch out for changes
-var styleSourceFile = '../teutonic-css/teutonic.scss';   // The one file including all imports
-var docsTargetDir   = 'docs/css';                        // Put the generated file here (development)
-var docsRoot        = 'docs';                            // Generated output of Jekyll during DEV
+const styleWatchDir   = '../teutonic-css/**/*.{css,scss}'; // All CSS & SCSS files to watch out for changes
+const styleSourceFile = '../teutonic-css/teutonic.scss';   // The one file including all imports
+const docsTargetDir   = 'docs/css';                        // Put the generated file here (development)
+const docsRoot        = 'docs';                            // Generated output of Jekyll during DEV
 
-
-// Header
-var pkg = require('./package.json');
-var banner = ['/**',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * @link <%= pkg.homepage %>',
-  ' * @license <%= pkg.license %>',
-  ' */',
-  ''].join('\n');
+const browserSyncConfig = {
+  files: [docsRoot + '/**/*'],
+  proxy: "127.0.0.1:4000",
+  port: 4000
+}
 
 // Generate CSS for docs during development
 // with sourcemaps and not minified
-gulp.task('sass', function () {
-  return gulp.src(styleSourceFile)
+function styles() {
+  return gulp
+    .src(styleSourceFile)
     .pipe(sourcemaps.init())
-      .pipe(sass.sync().on('error', sass.logError))
-      // .pipe(prefix({
-      //   browsers: ['last 1 version'],
-      //   cascade: false
-      // }))
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
     .pipe(cleanCSS())
-    //.pipe(header(banner, { pkg : pkg } ))
     .pipe(gulp.dest(docsTargetDir))
     .pipe(browserSync.stream());
-});
+}
 
-// Serve
-gulp.task('default', ['sass'], function() {
-  browserSync.init({
-    files: [docsRoot + '/**/*'],
-    proxy: "127.0.0.1:4000",
-    //server: docsRoot,
-    port: 4000
-  });
-  gulp.watch(styleWatchDir, ['sass']);
-});
+function watchStyles(done) {
+  gulp.watch(styleSourceFile, gulp.series('styles'));
+  done();
+}
+
+function browserSyncInit(done) {
+  browserSync.init(browserSyncConfig)
+  done();
+}
+
+// Tasks
+gulp.task('styles', styles);
+gulp.task('watchStyles', watchStyles)
+gulp.task('browserSyncInit', browserSyncInit);
+gulp.task('watch', gulp.parallel('styles'));
+gulp.task('default', gulp.series('styles', gulp.parallel('watch', 'browserSyncInit')));
